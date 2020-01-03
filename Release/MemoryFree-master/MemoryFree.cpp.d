7f82b1885ce3d0f49735c4e5e829fd0b45b79106
gerhard <gerhard@3it.at>
2020-01-03 18:45:02 +0100

    Aufräumarbeit

--- Release/MemoryFree-master/MemoryFree.cpp.d
+++
@@ -1,3 +0,0 @@
-C:\Users\markus\AppData\Local\Temp\VMBuilds\Control\mega_atmega2560\Release\MemoryFree-master\MemoryFree.cpp.o: \
- C:\Users\markus\Documents\Arduino\libraries\MemoryFree-master\MemoryFree.cpp \
- C:\Users\markus\Documents\Arduino\libraries\MemoryFree-master\MemoryFree.h
